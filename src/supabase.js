db24baa0b2e123c6860078b2e1100b50e75b52af
shivamxd <shivam.icecool@gmail.com>
2024-10-09 23:24:00 +0530

    removed my api key 💀💀

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://scgqnxwmgnlzokkngiog.supabase.co";
+const supabaseUrl = "database_url_here";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjZ3FueHdtZ25sem9ra25naW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgxMzc4MzEsImV4cCI6MjA0MzcxMzgzMX0.f7U8vikGcFfO2n29xBoyn3FrltZLjxSdRhTT5FL_7m4";
+  "your_key_here";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
